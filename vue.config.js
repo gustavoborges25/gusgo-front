2ac286ed56fb6265a4bc658c2dba22458be95a70
Gustavo Borges de Camargo <gustavo.camargo@uds.com.br>
2018-12-04 09:22:27 -0200

    Alteração na porta, para compatibilizar com o back.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 3000, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false,
+  },
 }
